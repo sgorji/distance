ae95ccf059513d614e42c14d19ad248694c4d8f0
sgorji <saeed.gorji@proton.me>
2023-01-20 15:32:36 +0100

    Generating random places

--- main.py
+++ main.py
@@ -2,12 +2,34 @@ import sys
 import numpy as np
 import pandas as pd
 
+# Geography
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
+
+geolocator = Nominatim(user_agent='Airmine')
+
 fileInputFlag = False
 
 
 def randomPlaces(n):
+    # randomly generated coordinates
+    # but this way does not generate an uniform distribution
+    places = []
+    while len(places) < n:
+        coordinates = (np.random.rand(n, 2) * [180, 360]) - [90, 180]
+        for latitude, longitude in coordinates:
+            coordinateString = str(latitude) + ',' + str(longitude)
+            location = geolocator.reverse(coordinateString)
+            if location:
+                try:
+                    name = location[0].split(',')[0]
+                    places.append([name, latitude, longitude])
+                except:
+                    print('no name at ' + coordinateString)
+
     print(f'Generate {n} random places.')
-    return
+    print(places)
+    return places
 
 
 argc = len(sys.argv)
